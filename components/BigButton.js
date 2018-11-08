4a54249e4688179bb57b92c2e3e8b8982dff455c
Luciano Muñoz <luchisds@gmail.com>
2018-11-08 18:00:09 -0300

    Create BigButton component to use in HomeScreen

--- components/BigButton.js
+++ components/BigButton.js
@@ -1,1 +1,59 @@
 import React from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+import {
+  Button,
+  Text,
+  TouchableRipple,
+  Surface,
+  withTheme
+} from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default class BigButton extends React.Component {
+  render() {
+    const label = this.props.label;
+    const elevation = this.props.elevation;
+    const radius = this.props.radius;
+    const icon = this.props.icon;
+    const iconSize = this.props.iconSize;
+    const iconColor = this.props.iconColor;
+
+    return (
+      <Surface style={[styles.button, {elevation: elevation, borderRadius: radius}]}>
+        <TouchableRipple borderless onPress={() => console.log('Pressed')}>
+           <View style={styles.content}>
+              <View>
+                <MaterialCommunityIcons name={icon} size={iconSize} color={iconColor} />
+              </View>
+              <Text
+                numberOfLines={1}
+                style={[styles.label]}>
+                {label}
+              </Text>
+           </View>
+        </TouchableRipple>
+      </Surface>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    minWidth: 64,
+    borderStyle: 'solid'
+  },
+  content: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  label: {
+    textAlign: 'center',
+    letterSpacing: 1,
+    marginVertical: 9,
+    marginHorizontal: 16
+  }
+});
--- navigation/HomeScreen.js
+++ navigation/HomeScreen.js
@@ -5,6 +5,7 @@ import {
   View
 } from 'react-native';
 import { Searchbar } from 'react-native-paper';
+import BigButton from '../components/BigButton';
 import { theme } from '../helpers/styles';
 
 export default class HomeScreen extends React.Component {
@@ -25,6 +26,8 @@ export default class HomeScreen extends React.Component {
         </View>
         <View>
           <View style={styles.row}>
+            <BigButton label='OK' elevation={3} radius={3} icon='refresh' iconSize={32} iconColor={theme.PRIMARY_COLOR} />
+            <BigButton label='OK' elevation={3} radius={3} icon='refresh' iconSize={32} iconColor='green' />
           </View>
           <View style={styles.row}>
           </View>
