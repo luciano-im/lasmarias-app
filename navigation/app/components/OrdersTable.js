9dd1ff9f17c454595ec33ebb3edba2e6703a03a0
Luciano Muñoz <luchisds@gmail.com>
2019-06-08 16:26:10 -0300

    Make orders fit in double-sized rows

--- navigation/app/components/OrdersTable.js
+++ navigation/app/components/OrdersTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TouchableOpacity, StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { IconButton, Text } from 'react-native-paper';
 import { ScaledSheet, moderateScale } from 'react-native-size-matters';
 import { format, parse } from 'date-fns';
 import { theme } from '../../../helpers/styles';
@@ -39,52 +39,68 @@ export default class OrdersTable extends React.Component {
     } else {
       if (type === 'Invoice') {
         headerContent = (
-          <View style={styles.row}>
-            <Text style={[styles.col1, styles.centerAlign, styles.title]}>
+          <View style={[styles.row, { flex: 1 }]}>
+            <Text
+              style={[styles.invoiceCol1, styles.centerAlign, styles.title]}
+            >
               Fecha
             </Text>
-            <Text style={[styles.col2, styles.centerAlign, styles.title]}>
+            <Text
+              style={[styles.invoiceCol2, styles.centerAlign, styles.title]}
+            >
               Nº
             </Text>
-            <Text style={[styles.col3, styles.title]}>Cliente</Text>
-            <Text style={[styles.col4, styles.centerAlign, styles.title]}>
+            <Text
+              style={[styles.invoiceCol3, styles.centerAlign, styles.title]}
+            >
               Importe
             </Text>
           </View>
         );
         itemsContent = data.map((item, index) => (
           <View style={styles.row} key={item.invoice_id}>
-            <Text style={[styles.col1, styles.dataText, styles.centerAlign]}>
-              {format(parse(item.date), 'DD/MM/YY')}
-            </Text>
-            <TouchableOpacity
-              style={styles.col2}
-              onPress={() =>
-                this.props.navigation.navigate('OrderDetail', {
-                  type: 'Invoice',
-                  data: item
-                })
-              }
-            >
-              <Text
-                style={[
-                  styles.centerAlign,
-                  styles.dataText,
-                  {
-                    color: theme.PRIMARY_COLOR,
-                    fontSize: moderateScale(14, 0.3)
-                  }
-                ]}
-              >
-                {item.invoice_id}
-              </Text>
-            </TouchableOpacity>
-            <Text style={[styles.col3, styles.dataText]}>
-              {item.customer_name}
-            </Text>
-            <Text style={[styles.col4, styles.dataText, styles.rightAlign]}>
-              $ {item.get_total.toFixed(2)}
-            </Text>
+            <View style={styles.container}>
+              <View style={styles.cleanRow}>
+                <Text style={styles.dataText}>{item.customer_name}</Text>
+              </View>
+              <View style={styles.cleanRow}>
+                <Text style={[styles.invoiceCol1, styles.detailDataText]}>
+                  {format(parse(item.date), 'DD/MM/YY')}
+                </Text>
+                <Text
+                  style={[
+                    styles.centerAlign,
+                    styles.invoiceCol2,
+                    styles.detailDataText
+                  ]}
+                >
+                  {item.invoice_id}
+                </Text>
+                <Text
+                  style={[
+                    styles.dataText,
+                    styles.rightAlign,
+                    styles.invoiceCol3
+                  ]}
+                >
+                  $ {item.get_total.toFixed(2)}
+                </Text>
+                <View style={styles.invoiceCol4}>
+                  <IconButton
+                    icon="search"
+                    color={theme.PRIMARY_COLOR}
+                    size={moderateScale(20, 0.3)}
+                    style={{ margin: 0 }}
+                    onPress={() =>
+                      this.props.navigation.navigate('OrderDetail', {
+                        type: 'Invoice',
+                        data: item
+                      })
+                    }
+                  />
+                </View>
+              </View>
+            </View>
           </View>
         ));
       } else {
@@ -96,45 +112,55 @@ export default class OrdersTable extends React.Component {
             <Text style={[styles.col2, styles.centerAlign, styles.title]}>
               Nº
             </Text>
-            <Text style={[styles.col3, styles.title]}>Cliente</Text>
-            <Text style={[styles.col4, styles.centerAlign, styles.title]}>
+            <Text style={[styles.col3, styles.centerAlign, styles.title]}>
               Solicitante
             </Text>
           </View>
         );
         itemsContent = data.map((item, index) => (
           <View style={styles.row} key={item.order_id}>
-            <Text style={[styles.col1, styles.dataText, styles.centerAlign]}>
-              {format(parse(item.date), 'DD/MM/YY')}
-            </Text>
-            <TouchableOpacity
-              style={styles.col2}
-              onPress={() =>
-                this.props.navigation.navigate('OrderDetail', {
-                  type: 'Order',
-                  data: item
-                })
-              }
-            >
-              <Text
-                style={[
-                  styles.centerAlign,
-                  styles.dataText,
-                  {
-                    color: theme.PRIMARY_COLOR,
-                    fontSize: moderateScale(14, 0.3)
-                  }
-                ]}
-              >
-                {item.order_id}
-              </Text>
-            </TouchableOpacity>
-            <Text style={[styles.col3, styles.dataText]}>
-              {item.customer_name}
-            </Text>
-            <Text style={[styles.col4, styles.dataText]}>
-              {item.user_customer_name}
-            </Text>
+            <View style={styles.container}>
+              <View style={styles.cleanRow}>
+                <Text style={styles.dataText}>{item.customer_name}</Text>
+              </View>
+              <View style={styles.cleanRow}>
+                <Text
+                  style={[
+                    styles.col1,
+                    styles.detailDataText,
+                    styles.centerAlign
+                  ]}
+                >
+                  {format(parse(item.date), 'DD/MM/YY')}
+                </Text>
+                <Text
+                  style={[
+                    styles.col2,
+                    styles.detailDataText,
+                    styles.centerAlign
+                  ]}
+                >
+                  {item.order_id}
+                </Text>
+                <Text style={[styles.col3, styles.dataText]}>
+                  {item.user_customer_name}
+                </Text>
+                <View style={styles.invoiceCol4}>
+                  <IconButton
+                    icon="search"
+                    color={theme.PRIMARY_COLOR}
+                    size={moderateScale(20, 0.3)}
+                    style={{ margin: 0 }}
+                    onPress={() =>
+                      this.props.navigation.navigate('OrderDetail', {
+                        type: 'Order',
+                        data: item
+                      })
+                    }
+                  />
+                </View>
+              </View>
+            </View>
           </View>
         ));
       }
@@ -161,11 +187,20 @@ const styles = ScaledSheet.create({
     borderBottomWidth: 1,
     paddingVertical: '5@ms0.3'
   },
+  cleanRow: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
   title: {
     fontWeight: theme.FONT_WEIGHT_MEDIUM,
     fontSize: '15@ms0.3'
   },
   dataText: {
+    fontSize: '14@ms0.3'
+  },
+  detailDataText: {
+    color: theme.PRIMARY_COLOR,
     fontSize: '14@ms0.3'
   },
   centerAlign: {
@@ -174,20 +209,33 @@ const styles = ScaledSheet.create({
   rightAlign: {
     textAlign: 'right'
   },
+  invoiceCol1: {
+    flex: 0.2
+  },
+  invoiceCol2: {
+    flex: 0.4
+  },
+  invoiceCol3: {
+    flex: 0.3
+  },
+  invoiceCol4: {
+    flex: 0.1,
+    alignItems: 'flex-end'
+  },
   col1: {
-    flex: 0.18
+    flex: 0.2
     // backgroundColor: '#EEE'
   },
   col2: {
-    flex: 0.2
+    flex: 0.3
     // backgroundColor: '#CCC'
   },
   col3: {
-    flex: 0.35
+    flex: 0.4
     // backgroundColor: '#AAA'
   },
   col4: {
-    flex: 0.27
+    flex: 0.1
     // backgroundColor: '#999'
   }
 });
